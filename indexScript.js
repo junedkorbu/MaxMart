3b0e02761b25fba45dcd8dfa8707db838b7cafff
junedkorbu <150371216+junedkorbu@users.noreply.github.com>
2024-04-02 15:32:40 +0530

    Add files via upload

---
+++ indexScript.js
@@ -0,0 +1,7 @@
+document.getElementById("register-btn").addEventListener("click", function() {
+    window.location.href = "Register.html";
+});
+
+document.getElementById("login-btn").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
